288388e0f6842f9945ceccdd008795ef8cfcfdb1
ChandraPrakashRawat <chandra.prakash.rawat@moreyeahs.in>
2023-07-05 14:36:53 +0530

    six

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,8 +20,8 @@ export class AppController {
   @Get('/oauth2/auth')
   salesforceOauth2(@Req() req:any,@Res() res:any){
     const oauth2 = new jsforce.OAuth2({
-      clientId: '3MVG9CecKwYCDceRMr3S_mwcNmu1rZP.1TPc3SFuEL9fR0FH.G3bkeYjJTrsLKFX9d53n5U6DhDsxnQR.Ilxn',
-      clientSecret: '5C5772466700A1C9BB8B91F7200E4C549DD683D1582EF6467CBE38BF8939D77C',
+      clientId: '3MVG9CecKwYCDceRMr3S_mwcNmnQ4e70OZ35.KsF2eSyogzu1epscVgLNTyV3CazSa6xZApWhIyaIUq7Uh_Pe',
+      clientSecret: '238BF2525C5AEC84B82E5A274FAE3895039D6B4F05BEBEBB879FC5D997D22807',
       redirectUri: `https://salesforceconnection.onrender.com/getAccessToken`
     });
     res.redirect(oauth2.getAuthorizationUrl({}));
